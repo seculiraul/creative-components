30797eb7078207993a44c6dc7e8293ecf3661d72
Raul <raulsmarius@yahoo.com>
2023-11-27 22:56:01 +0200

    small test for parallex

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router'
 import ButtonsMain from './components/buttons/ButtonsMain'
+import Homepage from './components/hompage/homepage'
 
 const App = () => {
   return (
-    <div className="h-screen bg-slate-900 ">
+    <div className="h-screen bg-slate-900">
       <Routes>
         <Route path="/buttons" element={<ButtonsMain />} />
+        <Route path="/home" element={<Homepage />} />
       </Routes>
     </div>
   )
