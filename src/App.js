dcc994bf781b64dc446570783ca2e0ca5a193fb7
Raul <raulsmarius@yahoo.com>
2023-11-28 21:39:29 +0200

    added a small navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router'
 import ButtonsMain from './components/buttons/ButtonsMain'
 import Homepage from './components/hompage/homepage'
+import Navbar from './components/navbar/navbar'
 
 const App = () => {
   return (
     <div className="h-screen bg-slate-900">
+      <Navbar />
       <Routes>
         <Route path="/buttons" element={<ButtonsMain />} />
         <Route path="/home" element={<Homepage />} />
